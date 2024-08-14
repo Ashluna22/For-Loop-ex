139c4fdbb2cc6d4420998ff41b354643cca43f46
Ashluna22 <ashleyvera032@gmail.com>
2024-08-14 02:16:24 -0400

    first push

---
+++ For-Loop-Ex/For-Loop-Ex/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+
+
+
+
+namespace For_Loop_Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                bool continueProgram = true;
+
+                while (continueProgram)
+                {
+                    Console.Write("Enter a number: ");
+                    int number = int.Parse(Console.ReadLine());
+
+                    for (int i = number; i >= 0; i--)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+
+                    for (int i = 0; i <= number; i++)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+
+                    Console.Write("Would you like to continue (y/n)? ");
+                    string response = Console.ReadLine().ToLower();
+
+                    if (response != "y")
+                    {
+                        continueProgram = false;
+                        Console.WriteLine("Goodbye!");
+                    }
+                }
+            }
+        }
+    }
+}
